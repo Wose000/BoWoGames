68e4f7826264895af7d5b6cc94276b9ff8e58072
Wose000 <jiro.due@gmail.com>
2023-12-20 17:22:50 +0100

    HELLOWORLD_UPDATE

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Helloworld();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Helloworld()
+    {
+        Debug.Log("HelloWORLD!");
+    }
+}
